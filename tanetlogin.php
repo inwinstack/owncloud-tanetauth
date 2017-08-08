9f9b2dd81c9e9fc86af1bf5799f35a821e61f504
Duncan Chiang <duncan.c@inwinstack.com>
2017-08-08 15:18:36 +0800

    [Feature] Design new tanet login page.

--- tanetlogin.php
+++ tanetlogin.php
@@ -43,12 +43,8 @@ if(!empty($_POST["account"]) || !empty($_POST["password"])) {
                     curl_close($ch);
                     
                     if ($result['ocs']['meta']['statuscode'] == 100){
-                        if ($result['ocs']['data']['result'] === true){
+                        if ($result['ocs']['data']['result'] == 'ture'){
                             $redirectHost= $result['ocs']['data']['host'];
-                        }
-                        else if ($result['ocs']['data']['host'] === false){
-                            $msg = "您沒有權限使用儲存應用服務!";
-                            break;
                         }
                     }
                     else{
@@ -56,7 +52,7 @@ if(!empty($_POST["account"]) || !empty($_POST["password"])) {
                         break;
                     }
                     
-                    $accountInfoArray = json_encode(['userid' => strtolower($userid),
+                    $accountInfoArray = json_encode(['userid' => $userid,
                                           'time' => time(),
                                           'password' => $password,
                                           'ip' => $ip
@@ -64,14 +60,14 @@ if(!empty($_POST["account"]) || !empty($_POST["password"])) {
                     ]);
 
                     $hash = hash('SHA384', $CONFIG['hash_key'], true);
-                    $aesKey = substr($hash, 0, 32);
-                    $aesIv = substr($hash, 32, 16);
+                    $app_cc_aes_key = substr($hash, 0, 32);
+                    $app_cc_aes_iv = substr($hash, 32, 16);
                     
-                    $accountInfoArray = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $aesKey, $accountInfoArray, MCRYPT_MODE_CBC, $aesIv);
-                    $encryptAccount = base64_encode($accountInfoArray);
+                    $accountInfoArray = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $app_cc_aes_key, $accountInfoArray, MCRYPT_MODE_CBC, $app_cc_aes_iv);
+                    $encrypt_account = base64_encode($accountInfoArray);
                     $params["tanet"] = true;
-                    $params["encrypt"] = $encryptAccount;
-                    $params["check"] = md5($encryptAccount . $CONFIG['crc_key']);
+                    $params["encrypt"] = $encrypt_account;
+                    $params["check"] = md5($encrypt_account . $CONFIG['crc_key']);
                     $queryStr = "?" . http_build_query($params);
 
                     $redirectUrl = 'https://' . $redirectHost . '/index.php' . $queryStr;
@@ -89,117 +85,34 @@ if(!empty($_POST["account"]) || !empty($_POST["password"])) {
     }
 }
 ?>
+<!DOCTYPE html>
 <html>
-<head>
+  <head>
     <meta charset="UTF-8">
-    <title>
-        雲端儲存服務        </title>
-    <link rel="shortcut icon" type="image/png" href="core/img/favicon.png">
-    <link rel="apple-touch-icon-precomposed" href="core/img/favicon-touch.png">
-    <link rel="stylesheet" href="core/css/styles.css" media="screen">
-    <link rel="stylesheet" href="core/css/header.css" media="screen">
-    <link rel="stylesheet" href="core/css/mobile.css" media="screen">
-    <link rel="stylesheet" href="core/css/icons.css" media="screen">
-    <link rel="stylesheet" href="core/css/fonts.css" media="screen">
+    <title>雲端儲存服務</title>
+    <link rel="stylesheet" href="apps/tanet_auth/css/login.css">
     
-    <link rel="stylesheet" href="core/css/apps.css" media="screen">
-    <link rel="stylesheet" href="core/css/fixes.css" media="screen">
-    <link rel="stylesheet" href="core/css/multiselect.css" media="screen">
-    <link rel="stylesheet" href="core/vendor/jquery-ui/themes/base/jquery-ui.css" media="screen">
-    <link rel="stylesheet" href="core/css/jquery-ui-fixes.css" media="screen">
-    <link rel="stylesheet" href="core/css/tooltip.css" media="screen">
-    <link rel="stylesheet" href="core/css/share.css" media="screen">
-    <link rel="stylesheet" href="core/css/jquery.ocdialog.css" media="screen">
-    
-    <link rel="stylesheet" href="themes/MOE/core/css/styles.css" media="screen">
-    <link rel="stylesheet" href="themes/MOE/core/css/header.css" media="screen">
-    <link rel="stylesheet" href="themes/MOE/core/css/icons.css" media="screen">
-    <link rel="stylesheet" href="themes/MOE/core/css/apps.css" media="screen">
-    <!--
-    <link rel="stylesheet" href="login/styles/vendor/bootstrap.css" />
-    <link rel="stylesheet" href="login/styles/vendor/font-awesome.css" />
-    -->
-</head>
-<body id="body-login">
-    <div class="wrapper">
-        <div class="v-align">
-            <header role="banner">
-                <div id="header">
-                    <div class="logo svg">
-                        <h1 class="hidden-visually">
-                            雲端儲存服務                                </h1>
-                    </div>
-                    <div id="logo-claim" style="display:none;"></div>
-                </div>
-            </header>
-                                
-            <form method="post" name="login">
-                <fieldset>
-                    <div id="message" class="hidden">
-                        <img class="float-spinner" alt="" src="/core/img/loading-dark.gif">
-                        <span id="messageText"></span>
-                        <div style="clear: both;"></div>
-                    </div>
-                    <p class="grouptop">
-                        <input type="text" name="account" id="user" placeholder="使用者名稱" value="" autofocus="" autocomplete="on" autocapitalize="off" autocorrect="off" required="">
-                        <label for="user" class="infield">使用者名稱</label>
-                    </p>
+  </head>
 
-                    <p class="groupbottom">
-                        <input type="password" name="password" id="password" value="" placeholder="密碼" autocomplete="on" autocapitalize="off" autocorrect="off" required="">
-                        <label for="password" class="infield">密碼</label>
-                        <input type="submit" id="submit" class="login primary icon-confirm svg" title="登入" value="">
-                    </p>
-
-                    <div class="remember-login-container">
-                        <input type="checkbox" name="remember_login" value="1" id="remember_login" class="checkbox checkbox--white">
-                        <label for="remember_login">remember</label>
-                    </div>
-                    <input type="hidden" name="timezone-offset" id="timezone-offset" value="8">
-                    <input type="hidden" name="timezone" id="timezone" value="Asia/Shanghai">
-                    <input type="hidden" name="requesttoken" value="aUsFDmwGEgcCbkMqFScfJC05JUgIfitlCkEGKDNg:02ITZ5TJC89dBkhrCts0b2q1fxLZY0">
-                </fieldset>
-            </form>
-            <div class="push">
-		<?php if(isset($msg)) echo "<p style='color:red;'>$msg</p>"; ?>
-	    </div>
-        </div>
+  <body>
+    <div><img class="resize"></img></div>
+    <div class="login-card">
+      <div class="logo"></img></div>
+      <form method="post" name="tanetlogin">
+        <input type="text" name="account" placeholder="TANet Roaming 帳號" required>
+        <input type="password" name="password" placeholder="TANet Roaming 密碼" required>
+        <input type="submit" name="login" class="login login-submit" value="登入">
+      </form>
+      <div class="push">
+        <?php if(isset($msg)) echo "<p style='color:red;'>$msg</p>"; ?>
+      </div>
     </div>
-    <footer role="contentinfo">
+    <div class="footer">
         <div class="footer-img"></div>
-        <div style="display: inline-block">
+        <div class="footer-text">
             請使用教育體系 TANet Roaming帳號進行登入<br>
             Copyright © Ministry of Education. All rigths reserved.
         </div>
-    </footer>
-<div class="container">
-        <div class="row">
-            <div class="col-sm-4 col-sm-offset-4">
-                <form name="login" class="panel panel-primary" method="POST">
-                    <input name="return_url" type="hidden" value="<?php //echo isset($_GET["returnUrl"]) ? $_GET["returnUrl"] : "";?>">
-                    
-                    <div class="panel-heading">
-                        <header class="panel-title">Sign In</header>
-                    </div>
-                    
-                    <div class="panel-body">
-                        <div class="form-group">
-                            <label for="account">Account</label>
-                            <input id="account" name="account" type="text" class="form-control" required />
-                        </div>
-                        
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input id="password" name="password" type="password" class="form-control" required />
-                        </div>
-                    </div>
-                    
-                    <div class="panel-footer">
-                        <button class="btn btn-default btn-block" type="submit">LogIn</button>
-                    </div>
-                </form>
-            </div>
-        </div>
     </div>
-</body>
+  </body>
 </html>
